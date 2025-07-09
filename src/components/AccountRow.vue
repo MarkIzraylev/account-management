e6c75247abca7e536fcfa337b8a02921d3f7c52e
Mark <markizraylev@yandex.ru>
2025-07-09 15:19:54 +0300

    Create basic layout

---
+++ src/components/AccountRow.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const password = ref()
+const showPassword = ref(false)
+const rules = {
+  required: (value) => !!value || 'Необходимо заполнить',
+  min: (v) => v.length >= 8 || 'Минимум 8 символов',
+}
+</script>
+
+<template>
+  <tr class="row">
+    <td>
+      <v-textarea
+        class="control"
+        label="Метки"
+        rows="1"
+        auto-grow
+        hint="Разделитель ;"
+      ></v-textarea>
+    </td>
+    <td>
+      <v-select class="control" label="Тип записи" :items="['LDAP', 'Локальная']"></v-select>
+    </td>
+    <td>
+      <v-text-field class="control" label="Логин"></v-text-field>
+    </td>
+    <td>
+      <v-text-field
+        class="control"
+        v-model="password"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[rules.required, rules.min]"
+        :type="showPassword ? 'text' : 'password'"
+        label="Пароль"
+        name="input-10-1"
+        counter
+        @click:append="showPassword = !showPassword"
+      ></v-text-field>
+    </td>
+    <td class="buttonCell">
+      <v-btn
+        density="default"
+        icon="mdi-trash-can-outline"
+        color="red-lighten-2"
+        variant="text"
+      ></v-btn>
+    </td>
+  </tr>
+</template>
+
+<style scoped>
+.row {
+  min-height: 200px;
+}
+
+.control {
+  min-width: 230px;
+}
+
+.buttonCell {
+  display: flex;
+  height: 56px;
+  align-items: center;
+  justify-content: center;
+}
+
+td {
+  white-space: nowrap;
+}
+</style>
---
+++ src/pages/AccountManagement.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import AccountRow from '@/components/AccountRow.vue'
+</script>
+
+<template>
+  <div class="main">
+    <div class="header">
+      <span class="heading">Учётные записи</span>
+      <v-btn icon="mdi-plus-circle-outline" color="blue-lighten-1" variant="text"></v-btn>
+    </div>
+    <v-alert
+      color="info"
+      icon="$info"
+      text="Для указания нескольких меток для одной пары логин/пароль, используйте разделитель ;"
+    ></v-alert>
+    <div class="accountsWrapper">
+      <table class="accounts">
+        <thead>
+          <th>Метки</th>
+          <th>Тип записи</th>
+          <th>Логин</th>
+          <th>Пароль</th>
+        </thead>
+        <tbody>
+          <AccountRow v-for="(row, ind) in new Array(3)" :key="ind" />
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 32px;
+  max-width: 1500px;
+  margin: 0 auto;
+  font-family:
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Open Sans',
+    'Helvetica Neue',
+    sans-serif;
+}
+
+.header {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+}
+
+.heading {
+  font-size: 20px;
+  font-weight: 600;
+  position: relative;
+  top: -2px;
+}
+
+.accountsWrapper {
+  overflow-x: auto;
+  border: 1px solid lightblue;
+  padding-top: 8px;
+  border-radius: 4px;
+}
+
+.accounts {
+  width: 100%;
+  border-spacing: 24px 8px;
+}
+</style>
